b22c0e4e3668760dcfc110f58017180c4870bdda
mailmenot69-ops <mailmenot69@gmail.com>
2026-02-01 21:45:01 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 
         let rawData = [];
         let currentSubject = 'overall';
-        const API =
+        const API = "https://ass-pee-rents-leaderboard.vercel.app";
         async function loadData() {
             const res = await fetch(`${API}/api/leaderboard`);
             rawData = await res.json();
